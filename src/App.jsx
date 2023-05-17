27b7d7ab2a5dc77ce6b79a0445f1f8a781e7b835
hasan469328 <hasanur.68.bd@gmail.com>
2023-05-17 23:00:33 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { Button } from "flowbite-react";
+
+function App() {
+  return (
+    <>
+      <Button color="light" pill={true}>
+        Light
+      </Button>
+      <Button color="purple" pill={true}>
+        Purple
+      </Button>
+    </>
+  );
+}
+
+export default App;
