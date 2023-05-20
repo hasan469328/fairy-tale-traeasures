58101fc1c02e74903d2789795765af7edd402168
hasan469328 <hasanur.68.bd@gmail.com>
2023-05-20 13:28:12 +0600

    add to home button added in 404 page

--- src/pages/NotFound/NotFound.jsx
+++ src/pages/NotFound/NotFound.jsx
@@ -1,5 +1,8 @@
+import { Button } from "flowbite-react";
+import { Link } from "react-router-dom";
 
 const NotFound = () => {
+  
   return (
     <div className="mx-auto max-w-md">
       <img
@@ -8,9 +11,10 @@ const NotFound = () => {
         className="block mx-auto w-3/4 h-auto mb-5"
       />
       <h1 className="text-3xl font-semibold text-center text-red-700 mb-5">404 - Page not found</h1>
-      <p className="text-center">
+      <p className="text-center mb-5">
         We&apos;re sorry, but the page you&apos;re looking for could not be found.
       </p>
+      <Button className="block mx-auto" gradientMonochrome="purple"><Link to="/">Back to Home</Link></Button>
     </div>
   );
 };
